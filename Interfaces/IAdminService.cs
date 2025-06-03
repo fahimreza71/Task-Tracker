2f6a9b6c9154b7517214ae8d146a8437ef9498bb
Fahim Reza <fahim.bdctg71@gmail.com>
2025-06-03 22:40:42 +0600

    added admin interface

---
+++ Interfaces/IAdminService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TaskTracker.Models;
+
+public interface IAdminService
+{
+    Task<IEnumerable<IdentityUser>> GetAllUsersAsync();
+    Task<IdentityUser?> GetUserByIdAsync(string userId);
+    Task<IEnumerable<TaskItem>> GetUserTasksAsync(string userId);
+    Task UpdateUserAsync(IdentityUser user);
+    Task DeleteUserAsync(string userId);
+}
